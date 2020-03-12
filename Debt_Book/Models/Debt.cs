68f474d439bbb7215a7580b26b734d7b76971725
GitDal <jeppedybdal@gmail.com>
2020-03-12 14:55:19 +0100

    Added fields and properties to Debt

--- Debt_Book/Models/Debt.cs
+++ Debt_Book/Models/Debt.cs
@@ -8,5 +8,23 @@ namespace Debt_Book.Models
 {
     public class Debt
     {
+        #region fields
+        private int _debtValue;
+        private readonly DateTime _date;
+        #endregion
+
+        #region properties
+
+        public int DebtValue
+        {
+            get => _debtValue;
+        }
+
+        public DateTime Date
+        {
+            get => _date;
+        }
+
+        #endregion
     }
 }
