18b2235e01991eff8ebd9b731fa4454cfaaa122c
GitDal <jeppedybdal@gmail.com>
2020-03-12 22:00:10 +0100

    Updates to XAML overview and MVVM overview

--- Debt_Book/ViewModels/OverviewWindowViewModel.cs
+++ Debt_Book/ViewModels/OverviewWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,44 @@ namespace Debt_Book.ViewModels
 {
     public class OverviewWindowViewModel : BindableBase
     {
-        private List<Debtor> _debtors;
+        private ObservableCollection<Debtor> _debtors;
+        private int _currentIndex = -1;
+        private Debtor _currentDebtor = null;
+        
 
         public OverviewWindowViewModel()
         {
-            _debtors = new List<Debtor>();
+            _debtors = new ObservableCollection<Debtor>
+            {
+                #if DEBUG
+                new Debtor("Magnus"),
+                new Debtor("Jeppe"),
+                new Debtor("Mikkel"),
+                new Debtor("Markus"),
+                new Debtor("Frederik"),
+                new Debtor("Nikolaj")
+                #endif
+            };
         }
 
         #region Properties
 
-        public List<Debtor> Debtors
+        public ObservableCollection<Debtor> Debtors
         {
             get => _debtors;
             set => SetProperty(ref _debtors, value);
+        }
+
+        public int CurrentIndex
+        {
+            get => _currentIndex;
+            set => SetProperty(ref _currentIndex, value);
+        }
+
+        public Debtor CurrentDebtor
+        {
+            get => _currentDebtor;
+            set => SetProperty(ref _currentDebtor, value);
         }
 
         #endregion
